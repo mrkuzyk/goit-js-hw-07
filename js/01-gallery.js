3b442b8fc1adc80430fcb5c590cc2deb7b0e667e
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-12 23:08:23 +0200

    create-html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+// cтворюю розмітку з вхідного масиву
+const makePreviewImageGallery = ({preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+}
+
+// перебираю масив і позбуваюся його
+//? const elements = galleryItems.map(makePreviewImageGallery).join('');
+// добавляю розмітку в готовий хтмл
+//? galleryEl.innerHTML = elements;
+
+galleryEl.innerHTML = galleryItems.map(makePreviewImageGallery).join('');
